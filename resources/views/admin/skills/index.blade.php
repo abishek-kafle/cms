4a6453ec6d9663d62dbadec61759e5d4401dcc73
abishek kafle <abishek.kafle@gmail.com>
2022-03-23 09:58:39 +0545

    skill index page on work

---
+++ resources/views/admin/skills/index.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.includes.admin_design')
+
+@section('content')
+<div class="page-wrapper">
+
+    <div class="content container-fluid">
+
+        <div class="card">
+            @include('admin.includes._messages')
+            <div class="card-header">
+                <h5 class="card-title mb-0">Admin Profile</h5>
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
